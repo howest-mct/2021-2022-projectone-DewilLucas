30a6eee69526ea6acfe4171dc6fc9bf7e9c5071c
DewilLucas <lucas.dewil@student.howest.be>
2022-06-02 13:18:52 +0200

    ok

--- backend/classes/stats.py
+++ backend/classes/stats.py
@@ -35,7 +35,7 @@ image = Image.new("1", (oled.width, oled.height))
 
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
-
+#image = Image.open('happycat_oled_64.ppm').convert('1')
 # Draw a white background
 draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
 
@@ -48,12 +48,23 @@ while True:
     draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
 
     # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.check_output(cmd, shell=True)
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell=True)
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell=True)
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell=True)
     cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
     temp = subprocess.check_output(cmd, shell=True)
 
     # Pi Stats Display
-    draw.text((0, 0), str(temp, 'utf-8'), font=font, fill=255)
+    draw.text((0, 0), "IP: " + str(IP, 'utf-8'), font=font, fill=255)
+    draw.text((0, 16), str(CPU, 'utf-8') + "%", font=font, fill=255)
+    draw.text((80, 16), str(temp, 'utf-8'), font=font, fill=255)
+    draw.text((0, 32), str(MemUsage, 'utf-8'), font=font, fill=255)
+    draw.text((0, 48), str(Disk, 'utf-8'), font=font, fill=255)
 
     # Display image
     oled.image(image)
